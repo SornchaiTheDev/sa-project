4d18ccbdc3c0d58ade328c23fd925983ecd22977
SornchaiTheDev <sornchaithedev@gmail.com>
2024-09-26 12:59:19 +0700

    chore: update onboarding page layout

--- src/app/(authed)/(half-layout)/onboarding/page.tsx
+++ src/app/(authed)/(half-layout)/onboarding/page.tsx
@@ -3,13 +3,13 @@ import UserInfoForm from "./_components/user-info-form";
 
 function OnBoardingPage() {
   return (
-    <>
+    <div className="mt-20">
       <h4 className="text-xl font-semibold">ลงทะเบียน</h4>
       <div className="mt-4 w-full">
         <h6 className="text-lg">ข้อมูลส่วนตัว</h6>
         <UserInfoForm />
       </div>
-    </>
+    </div>
   );
 }
 
