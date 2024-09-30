6f19fc82311eb2b56591145dad640e7c20d4da1b
SornchaiTheDev <sornchaithedev@gmail.com>
2024-09-30 13:49:21 +0700

    fix: added phone regex in user info form

--- src/app/(authed)/(half-layout)/onboarding/schemas/user-info.ts
+++ src/app/(authed)/(half-layout)/onboarding/schemas/user-info.ts
@@ -12,7 +12,8 @@ export const userInfo = z.object({
   phone: z
     .string()
     .min(9, "เบอร์ติดต่อต้องมีอย่างน้อย 9 หลัก")
-    .max(10, "เบอร์ติดต่อต้องมีไม่เกิน 10 หลัก"),
+    .max(10, "เบอร์ติดต่อต้องมีไม่เกิน 10 หลัก")
+    .regex(/^[0-9]{9,10}$/, "กรุณากรอกเบอร์ติดต่อให้ถูกต้อง"),
 });
 
 export type UserInfo = z.infer<typeof userInfo>;
