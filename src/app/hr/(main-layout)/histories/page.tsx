08e865c8c0aadef43754fdce19699b8340c8325e
SornchaiTheDev <sornchaithedev@gmail.com>
2024-10-06 02:55:35 +0700

    feat(hr): implemented histories page

---
+++ src/app/hr/(main-layout)/histories/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HistoryPage() {
+  return (
+    <div>
+      <h3 className='text-xl'>ประวัติการรับสมัครงาน</h3>
+      <h4 className='text-lg'>XXX คน</h4>
+    </div>
+  )
+}
+
+export default HistoryPage
