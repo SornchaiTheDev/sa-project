825852dff1b9c3b69cff1537db152d7257fb47e6
SornchaiTheDev <sornchaithedev@gmail.com>
2024-10-09 00:04:27 +0700

    chore: added update student DTO

--- src/backend/DTO/studentDTO.ts
+++ src/backend/DTO/studentDTO.ts
@@ -5,3 +5,17 @@ export interface CreateStudent {
   lastName: string;
   email: string;
 }
+
+export interface UpdateStudent {
+  prefix: string;
+  firstName: string;
+  surName: string;
+  email: string;
+  bod: Date;
+  phone: string;
+  faculty: string;
+  major: string;
+  gpax: string;
+  activitiesHours: string;
+  workExp: string;
+}
