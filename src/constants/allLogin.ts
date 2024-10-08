ce5ce8d935ace542fcb748dd9a781b4158987ccc
SornchaiTheDev <sornchaithedev@gmail.com>
2024-10-08 21:40:35 +0700

    chore: made ku all login uri as constants

---
+++ src/constants/allLogin.ts
@@ -0,0 +1,8 @@
+export const KU_ALL_LOGIN_AUTHORIZATION_URI =
+  "https://sso-dev.ku.ac.th/realms/KU-Alllogin/protocol/openid-connect/auth";
+export const KU_ALL_LOGIN_USER_INFO_URI =
+  "https://sso-dev.ku.ac.th/realms/KU-Alllogin/protocol/openid-connect/userinfo";
+export const KU_ALL_LOGIN_TOKEN_ENDPOINT =
+  "https://sso-dev.ku.ac.th/realms/KU-Alllogin/protocol/openid-connect/token";
+export const KU_ALL_LOGIN_END_SESSION_ENDPOINT =
+  "https://sso-dev.ku.ac.th/realms/KU-Alllogin/protocol/openid-connect/logout";
