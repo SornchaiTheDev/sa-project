c4a9dc960e285032045a4a7df88f10519990ecbf
SornchaiTheDev <sornchaithedev@gmail.com>
2024-10-08 13:57:03 +0700

    chore: used a tag instead of Link

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
-import Link from "next/link";
 import { KULogo } from "~/configs/assets";
 
 function NisitSignInPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h2 className="text-lg font-medium text-center">เข้าสู่ระบบ</h2>
-      <Link
+      <a
         href="/api/auth/kusso/sign-in"
         className="flex justify-center items-center rounded-lg text-sm w-full max-w-[400px] py-1 gap-4 border hover:bg-zinc-100 mt-4"
       >
         <Image src={KULogo} width={36} height={36} alt="KU Logo" />
         เข้าสู่ระบบด้วย KU-All Login
-      </Link>
+      </a>
     </div>
   );
 }
