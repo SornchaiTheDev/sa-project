fbb77b4de4c2c9e0401eaa6d045c4811fef58450
SornchaiTheDev <sornchaithedev@gmail.com>
2024-10-08 21:41:06 +0700

    chore: used env instead of process.env

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,13 @@
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { Pool } from "pg";
+import { env } from "~/configs/env";
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
-  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: env.POSTGRES_USER,
+  host: env.POSTGRES_HOST,
+  database: env.POSTGRES_DB,
+  password: env.POSTGRES_PASSWORD,
+  port: parseInt(env.POSTGRES_PORT || "5432", 10),
+  ssl: false,
 });
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
