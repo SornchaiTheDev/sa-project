c76a1059eebc53b6c44edf0694cc0a90db6fedb0
SornchaiTheDev <sornchaithedev@gmail.com>
2024-10-11 15:44:18 +0700

    chore: updated hr waiting page

--- src/app/hr/onboarding/waiting/page.tsx
+++ src/app/hr/onboarding/waiting/page.tsx
@@ -14,13 +14,13 @@ function WaitingListPage() {
         <h5 className="text-xl font-medium">รอดำเนินการอนุมัติ</h5>
         <Loader size="3rem" className="animate-spin" />
         <p className="text-center">
-          ระบบจะแจ้งเตือนไปยัง <br /> อีเมลล์ของคุณเมื่อคุณได้รับอนุมัติ
+          กรุณาเข้าสู่ระบบเพื่อตรวจสอบ <br /> สถานะการเข้่าใช้งานอีกครั้ง
         </p>
         <Link
           href="/hr/auth/sign-in"
           className="w-3/4 bg-primary rounded-lg flex justify-center items-center py-1 text-white mt-4"
         >
-          กลับสู่หน้าแรก
+          กลับสู่หน้าเข้าสู่ระบบ
         </Link>
       </motion.div>
     </div>
