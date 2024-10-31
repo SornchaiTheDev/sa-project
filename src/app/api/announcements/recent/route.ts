5de07c579afdb3b4a9691010f8c17cb6e8fc3823
SornchaiTheDev <sornchaithedev@gmail.com>
2024-10-31 23:22:40 +0700

    fix: api route build failed

--- src/app/api/announcements/recent/route.ts
+++ src/app/api/announcements/recent/route.ts
@@ -1,5 +1,7 @@
 import { getRecentJobAnnouncements } from "~/backend/models/jobAnnouncement-model";
 
+export const dynamic = "force-dynamic";
+
 export const GET = async () => {
   const announcements = await getRecentJobAnnouncements();
 
