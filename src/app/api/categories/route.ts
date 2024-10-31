9d7c17de020463e8b8bbb25bcf5f3b61df67e2b7
SornchaiTheDev <sornchaithedev@gmail.com>
2024-10-31 14:55:07 +0700

    feat: add dynamic export in category route

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,5 +1,7 @@
 import { getAllTags } from "~/backend/models/tag-model";
 
+export const dynamic = "forcce-dynamic";
+
 export const GET = async () => {
   const categories = await getAllTags();
   return Response.json({
